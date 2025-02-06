ff1d5751d1df47af6c604348d20f41f678c592d7
Maxim Melnikov <melnikovmaxim.nhk@gmail.com>
2025-02-06 10:29:55 +0300

    form-status hook

--- src/app/use-form-status-component/page.tsx
+++ src/app/use-form-status-component/page.tsx
@@ -1,12 +1,22 @@
 'use client'
 
+import { Button } from '@radix-ui/themes'
 import { useFormStatus } from 'react-dom'
 
 export default function Page() {
 	const { pending } = useFormStatus()
+
+	const submitForm = async _ => {
+    await new Promise((res) => setTimeout(res, 1000));
+	}
+
 	return (
-		<button type='submit' disabled={pending}>
-			Submit
-		</button>
+		<div>
+			<form action={submitForm}>
+				<Button type='submit' disabled={pending}>
+					Submit
+				</Button>
+			</form>
+		</div>
 	)
 }
